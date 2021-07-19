1b9f3e7d70624c0573b310b9cb9cd065a79f8f18
“Febrianto0202” <singalada@gmail.com>
2021-07-19 10:03:58 +0700

    some descriptive message

---
+++ build/precache-manifest.de95a658e593d51ffe5377d34d74f668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d23b8bc6ba766e526e57f2de90e7966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23a73f221fe8eb3dfc3f",
+    "url": "/static/css/main.8967ed85.chunk.css"
+  },
+  {
+    "revision": "b5b84fadf329bba4622c",
+    "url": "/static/js/2.3d7e441b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d7e441b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a73f221fe8eb3dfc3f",
+    "url": "/static/js/main.7675b251.chunk.js"
+  },
+  {
+    "revision": "4a50a5ef7e5f9af8745a",
+    "url": "/static/js/runtime-main.0ae9ac90.js"
+  }
+]);
